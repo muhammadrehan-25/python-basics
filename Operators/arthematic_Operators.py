5a676b89e24229e50c702f589a1c935ce78f3f6e
muhammadrehan-25 <engrehan21@gmail.com>
2026-07-24 20:29:18 +0500

    Complete Arithmetic Operators Practice

---
+++ Operators/arthematic_Operators.py
@@ -0,0 +1,156 @@
+"""Q1 Store two numbers in variables and print:
+1.Addition
+2.Subtraction
+3.Multiplication
+4.Division"""
+
+"""num1=15
+num2=12
+print("num1 = ",num1)
+print("num2 = ",num2)
+print("Addition: ",num1+num2)
+print("Subtraction: ",num1-num2)
+print("Multiplication: ",num1*num2)
+print("Devision: ",num1/num2)
+"""
+
+"""
+Q2: Take two numbers using input() and print:
+
+Sum
+Difference
+Product
+Quotient
+
+firstNum=int(input("Enter your first Number: "))
+secNumber=int(input("Enter your Seconed Number: "))
+
+print("Sum= ",firstNum+secNumber)
+print("Difference= ",firstNum-secNumber)
+print("Product= ",firstNum*secNumber)
+print("Quotient= ",firstNum/secNumber
+
+Q3: Take two integers and print:
+
+1.Floor Division
+2.Modulus
+
+print("floor Division = ",firstNum//secNumber)
+print("Modulus = ",firstNum%secNumber)
+
+Q4: Take one number and print its square using **.
+print("Square = ",firstNum**2)
+
+Q5: Take one number and print its cube.
+print("Cube",firstNum**3)"""
+
+
+"""Q6:Calculate the area of a rectangle.
+    
+width=float(input("Enter Width Of rectangle: "))
+length=float(input("Enter length of rectangle: "))
+area=length*width
+print("Area of Rectangle = ",area)
+
+Q7:Calculate the perimeter of a rectangle.
+Perimeter=2*(length+width)
+print("Perimeter of rectangle = ",Perimeter)"""
+
+"""Q8:Calculate the area of a circle.
+radius=float(input("Enter radius of circle: "))
+Area=3.142*radius**2
+print("Area of circle = ",Area) """
+
+"""
+Q9:Take marks of 5 subjects.
+
+Calculate:
+
+Total Marks
+Percentage
+
+sub1=45
+sub2=57
+sub3=78
+sub4=87
+sub5=89
+total=500
+marks_Obtained=sub1+sub2+sub3+sub4+sub5
+percentage=marks_Obtained/total*100
+
+print("obtained marks= ",marks_Obtained,"/",total)
+print("Percentage= ",percentage,"%")"""
+
+"""
+Q10:Take your monthly salary.
+Calculate yearly salary.
+
+salary_month=55000
+print("Monthly salary= ",salary_month)
+print("Salary in year= ",salary_month*12)"""
+
+"""Q11:Take total seconds from user.
+Convert into:
+
+Hours
+Minutes
+Seconds
+
+sec=int(input("Enter Seconeds: "))
+
+hours=sec//3600
+minute=(sec%3600)//60
+seconeds=sec%60
+
+print("Hours=",hours)
+print("Minutes=",minute)
+print("Seconds=",seconeds)"""
+
+"""Q12:Swap two numbers:
+Without using a third variable.
+
+a=15
+b=10
+print("a=",a)
+print("b=",b)
+a=a+b
+b=a-b
+a=a-b
+print("a =",a)
+print("b =",b)
+"""
+
+"""Q13:Take two numbers.
+Print: Average = ?
+
+a=8
+b=9
+print("a=",a,",b=",b)
+sum=a+b
+Average=sum/2
+print("Average of two number is ",Average)
+"""
+
+"""
+Q14:Calculate simple interest.
+
+P=50000
+R=5
+T=10
+
+SI=(P*R*T)/100
+Amount=P+SI
+print(f"Simple Interst {SI}")
+print("Total Return after simple interst ",Amount)"""
+
+distance=450
+fuel=24
+milage=distance/fuel
+print("Milage =",milage)
+
+# ===============================
+# Completed By:
+# Muhammad Rehan
+# Date:24/07/2026
+# Topic: Arithmetic Operators
+# ===============================
