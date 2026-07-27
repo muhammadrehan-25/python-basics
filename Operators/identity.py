2f9ba517303dbff71334e3aa7059d99aebaf739a
muhammadrehan-25 <engrehan21@gmail.com>
2026-07-27 23:05:29 +0500

    Complete Membership Operators and Identity Operators

---
+++ Operators/identity.py
@@ -0,0 +1,35 @@
+#========== Identity Operator ===========
+
+# Question 1
+
+a=100
+b=100
+print(a==b)
+print(a is b)
+
+# Question 2
+
+c=[20]
+d=[20]
+print(c==d)
+print(c is d)
+
+# Question 3
+
+mylist_1=[100]
+mylist_2=mylist_1
+print(mylist_1 is mylist_2)
+
+# Question 4
+
+q='This Is Apple'
+r='I m Rehan'
+
+print(q  is not r)
+
+# Question 5
+
+var_1 =None
+var_2=None
+
+print(var_1 is var_2)
---
+++ Operators/membership_operator.py
@@ -0,0 +1,49 @@
+# ========= Membership Operator =============
+
+#Question 1
+
+student_name="Rehan"
+print("Rehan in sring?: ",'Rehan' in student_name)
+print("Bilal in string?: ",'Bilal' in student_name)
+
+# Question 2
+print()
+numers=[5,10,15,20,25]
+print("15 in the list: ",15 in numers)
+print("100 not in the list: ",100 not in numers)
+
+# Question 3
+
+print()
+collors=['Blue','Red','Green']
+print("Yellow not in the collor list: ",'Yellow' not in collors)
+
+# Question 4
+
+print()
+city='Karachi'
+print("chi in Karachi :",'chi' in city)
+print("lah in Karachi",'lah' in city)
+
+# Question 5
+
+print()
+food=['Biryani','Quorma','Custer','Bhindi','Kheer']
+print("Custer in the food list: ",'Custer' in food)
+print("Karella in the food list: ",'Karella' in food)
+print("Biryani in the food list: ",'Biryani' in food)
+print("Halwa not in the food list: ",'Halwa' not in food)
+print("Bhindi not in the food list: ",'Bhindi' not in food)
+
+#challenge 1
+
+print()
+programming_languages=['Java','C++','C#','HTML','Python']
+print("Python in the programming languages list: ",'Python' in programming_languages)
+
+#Challenge 2
+
+print()
+username='rehan321'
+print("321 in username:",'321' in username)
+print("123 is not in the username: ",'123' not in username)
