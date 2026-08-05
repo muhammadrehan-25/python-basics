e2ed9baae0ad574f5b50dd78dc347498f4a1756f
muhammadrehan-25 <engrehan21@gmail.com>
2026-08-05 23:39:09 +0500

    Completed Nested loops

--- Control flow/Nested_loop.py
+++ Control flow/Nested_loop.py
@@ -22,7 +22,38 @@ for i in range(3):
 print()
 
 # Example 2
-for i in range(1, 5):
-    for j in range(i):
-        print("*", end=" ")
+for i in range(5,0,-1):
+    for j in range(1,i+1):
+        print(i, end=" ")
+    print()
+    
+print()
+for i in range(1,6,):
+    for j in range(1,i+1):
+        print(i, end=" ")
+    print()
+
+print()    
+for i in range(5,0,-1):
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print()
+
+print()
+for i in range(6) :
+    for j in range(i,0,-1):
+        print(j,end=" ")
+    print()
+print() 
+  
+for i in range(1,6):
+    for j in range(5, 5-i, -1):
+        print(j, end=" ")
+    print()
+    
+for i in range(1,6):
+    for j in range(5-i):
+        print(" ",end="")
+    for k in range(i):
+        print("*",end="")
     print()
