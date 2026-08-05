167c070053b04d5a4470c4d0dbadc2a2137f43cb
muhammadrehan-25 <engrehan21@gmail.com>
2026-08-06 00:07:24 +0500

    REmove test 1 sql file and teestruner.py file

--- Control flow/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-
-        
-#Question 7 (Medium)
-
-#Print each student's name with its roll number.
-
-students = ["Ali", "Ahmed", "Rehan", "Bilal"]
-for index,student in enumerate(students,start=1):
-    print(f"Roll Number {index} : {student}")
