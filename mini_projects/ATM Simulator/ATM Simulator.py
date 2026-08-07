eb34c89964068821279f5fe79af0aa8b8e39cd51
muhammadrehan-25 <engrehan21@gmail.com>
2026-08-08 00:26:59 +0500

    Add Pin verification and do some basic changes

--- mini_projects/ATM Simulator/ATM Simulator.py
+++ mini_projects/ATM Simulator/ATM Simulator.py
@@ -1,52 +1,79 @@
 history =[]
 balance  = 5000
 amount = 0
-while True:
-    print("===================================")
-    print("           ATM SIMULATOR           ")
-    print("===================================")
-    
-    print("1.Check Balance.")
-    print("2.Deposit Money.")
-    print("3.Withdraw Money.")
-    print("4.Transaction History .")
-    print("5.Exit")
-    print("===================================")
-    choice = int(input("Enter choice: "))
-    if choice == 5:   
-        print("Thanku for using ATM.")
-        print("-" * 35)
+pin =1234
+login = False
+for i in range(3):
+    user_pin=int(input("Enter pin to login: "))
+    if user_pin == pin:
+        print("-"*35)
+        print("Login Successfully")
+        print("-"*35)
+        login = True 
         break
-    if choice == 1:
-        if not balance:
-            print("Inffucient Balance,")
-        else:
-            print(f"Yor balance is: {balance}")
-    elif choice == 2:
-        amount = int (input("Enter amount to deposit."))
-        balance+=amount
-        history.append(f"Deposit amount: {amount}")
-        print("Deposit successfully.")
 
-    elif choice == 3:
-        amount=int (input("Enter amount to withdraw"))
-        if amount>balance:
-            print("Insufficient balance.")
-            
-        else:
-            balance-=amount
-            history.append(f"Withdraw amount: {amount}")
-            print("Withdraw Successfully.")
-    elif choice == 4:
-        if not history:
-            print("No Historie Found.")
-        else:
-            for index,transaction in enumerate(history,start=1):
-                 print(f"{index}.{transaction}")
     else:
-        print("Invalid choice.")  
+        print("Invalid Pin.")
+if login:    
+    while True:
         
-        
-        
-        
-    
+            print("===================================")
+            print("           ATM SIMULATOR           ")
+            print("===================================")
+            
+            print("1.Check Balance.")
+            print("2.Deposit Money.")
+            print("3.Withdraw Money.")
+            print("4.Transaction History .")
+            print("5.Exit")
+            print("===================================")
+            choice = int(input("Enter choice: "))
+            if choice == 5:   
+                print("Thanku for using ATM.")
+                print("-" * 35)
+                break
+            if choice == 1:
+                if  balance<=0:
+                    print("Inffucient Balance,")
+                    print()
+                else:
+                    print(f"Yor balance is: {balance}")
+                    print()
+            elif choice == 2:
+                amount = int (input("Enter amount to deposit: "))
+                if amount>=0:
+                    balance+=amount
+                    history.append(f"Deposit amount: {amount}")
+                    print("Deposit successfully.")
+                else:
+                    print("Amount must be greater then 0.")
+                
+
+            elif choice == 3:
+                amount=int (input("Enter amount to withdraw"))
+                if amount<=0:
+                    print("Amount must be greater then 0")
+                elif amount>balance:
+                    print("Insufficient balance.")
+                    print()
+                    
+                else:
+                    balance-=amount
+                    history.append(f"Withdraw amount: {amount}")
+                    print("Withdraw Successfully.")
+                    print()
+            elif choice == 4:
+                if not history:
+                    print("No Historie Found.")
+                    print()
+                else:
+                    for index,transaction in enumerate(history,start=1):
+                        print(f"{index}.{transaction}")
+                        print()
+            else:
+                print("Invalid choice.")  
+                print()
+                
+                
+                
+            
